7abe2019ac5aa9d1981372e1737732dc5808cea0
endercan ergin <87834304+enderergin@users.noreply.github.com>
2023-06-04 23:26:56 +0300

    Add files via upload

---
+++ formapp05/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace formapp05
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTemp = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnTemp
+            // 
+            this.btnTemp.Location = new System.Drawing.Point(688, 12);
+            this.btnTemp.Name = "btnTemp";
+            this.btnTemp.Size = new System.Drawing.Size(100, 142);
+            this.btnTemp.TabIndex = 0;
+            this.btnTemp.Text = "Üret";
+            this.btnTemp.UseVisualStyleBackColor = true;
+            this.btnTemp.Click += new System.EventHandler(this.btnTemp_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 1);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(679, 163);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 234);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnTemp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dinamik form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTemp;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ formapp05/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formapp05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnTemp_Click(object sender, EventArgs e)
+        {
+            int mayin1 = 0;
+            int mayin2 = 0;
+            int mayin3 = 0;
+            Random rnd = new Random();
+            mayin1 = rnd.Next(1, 20);
+            mayin2 = rnd.Next(21, 30);
+            mayin3 = rnd.Next(31, 50);
+
+            for (int i = 1; i <= 50; i++)
+            { 
+                Button butoUret = new Button();
+                butoUret.Name = "BTN" + i.ToString();
+                butoUret.Size = new Size(27,27);
+                butoUret.Text = i.ToString();
+                butoUret.UseVisualStyleBackColor = true;
+                if (mayin1 == i || mayin2 == i || mayin3 == i)
+                {
+                    butoUret.Tag = true;
+                }
+                else
+                {
+                    butoUret.Tag = false;
+                }
+                butoUret.Tag = null;
+                butoUret.Click += btnMayin;
+                flowLayoutPanel1.Controls.Add(butoUret);
+            }
+        
+        }
+        private void btnMayin (object sender, EventArgs e)
+        {
+            Button basilanbuton = ((Button)sender);
+            bool ButonBulundumu = (bool)basilanbuton.Tag;
+            if (ButonBulundumu) 
+            {
+                MessageBox.Show("Mayına bastınız");
+                basilanbuton.BackColor = Color.Red;
+            }
+            else
+            {
+
+                basilanbuton.BackColor= Color.Green;
+            }
+        }
+    }
+}
