6c6ffb249af2c4adf8ba5d469ea35a7e0b55659b
endercan ergin <87834304+enderergin@users.noreply.github.com>
2023-06-17 17:09:35 +0300

    chklistbox değerleri

---
+++ formApp08/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace formApp08
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkListBox = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAcıklama = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtYazar = new System.Windows.Forms.TextBox();
+            this.StokAdet = new System.Windows.Forms.TextBox();
+            this.Katogori = new System.Windows.Forms.TextBox();
+            this.UrunAdi = new System.Windows.Forms.TextBox();
+            this.Resim = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Resim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkListBox
+            // 
+            this.chkListBox.FormattingEnabled = true;
+            this.chkListBox.Location = new System.Drawing.Point(12, 41);
+            this.chkListBox.Name = "chkListBox";
+            this.chkListBox.Size = new System.Drawing.Size(268, 327);
+            this.chkListBox.TabIndex = 0;
+            this.chkListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.chkListBox_ItemCheck);
+            this.chkListBox.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAcıklama);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtYazar);
+            this.groupBox1.Controls.Add(this.StokAdet);
+            this.groupBox1.Controls.Add(this.Katogori);
+            this.groupBox1.Controls.Add(this.UrunAdi);
+            this.groupBox1.Controls.Add(this.Resim);
+            this.groupBox1.Location = new System.Drawing.Point(299, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(509, 354);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ürün detay bilgisi";
+            // 
+            // txtAcıklama
+            // 
+            this.txtAcıklama.Location = new System.Drawing.Point(6, 155);
+            this.txtAcıklama.Name = "txtAcıklama";
+            this.txtAcıklama.Size = new System.Drawing.Size(327, 134);
+            this.txtAcıklama.TabIndex = 6;
+            this.txtAcıklama.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Yazar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Stok Adet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Katoğori";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ürüm Adı";
+            // 
+            // txtYazar
+            // 
+            this.txtYazar.Location = new System.Drawing.Point(191, 105);
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(158, 22);
+            this.txtYazar.TabIndex = 4;
+            // 
+            // StokAdet
+            // 
+            this.StokAdet.Location = new System.Drawing.Point(191, 77);
+            this.StokAdet.Name = "StokAdet";
+            this.StokAdet.Size = new System.Drawing.Size(158, 22);
+            this.StokAdet.TabIndex = 4;
+            // 
+            // Katogori
+            // 
+            this.Katogori.Location = new System.Drawing.Point(191, 49);
+            this.Katogori.Name = "Katogori";
+            this.Katogori.Size = new System.Drawing.Size(158, 22);
+            this.Katogori.TabIndex = 4;
+            // 
+            // UrunAdi
+            // 
+            this.UrunAdi.Location = new System.Drawing.Point(191, 21);
+            this.UrunAdi.Name = "UrunAdi";
+            this.UrunAdi.Size = new System.Drawing.Size(158, 22);
+            this.UrunAdi.TabIndex = 4;
+            // 
+            // Resim
+            // 
+            this.Resim.Location = new System.Drawing.Point(6, 21);
+            this.Resim.Name = "Resim";
+            this.Resim.Size = new System.Drawing.Size(100, 128);
+            this.Resim.TabIndex = 3;
+            this.Resim.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(411, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(308, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ürün Listesi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 430);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Checkled List Box";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Resim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox chkListBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtAcıklama;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYazar;
+        private System.Windows.Forms.TextBox StokAdet;
+        private System.Windows.Forms.TextBox Katogori;
+        private System.Windows.Forms.TextBox UrunAdi;
+        private System.Windows.Forms.PictureBox Resim;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ formApp08/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formApp08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void checkedListBox1Doldur()
+        {
+            foreach (var item in BaseClass.list)
+            {
+                chkListBox.Items.Add(item);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkedListBox1Doldur();
+
+        }
+
+        private void chkListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            CheckedListBox c1 = (CheckedListBox)sender;
+            if (e.NewValue == CheckState.Checked)
+            {
+                Urun secilenUrun = c1.Items[e.Index] as Urun;
+                Resim.Image = Image.FromFile(secilenUrun.urunResim);
+                Resim.SizeMode = PictureBoxSizeMode.StretchImage;
+                UrunAdi.Text = secilenUrun.urunADİ;
+                Katogori.Text = secilenUrun.urunKatogori;
+                StokAdet.Text = secilenUrun.urunAdet.ToString();
+                txtYazar.Text = secilenUrun.yazar;
+                txtAcıklama.Text = secilenUrun.acıklama;
+
+            }
+        }
+    }
+}
